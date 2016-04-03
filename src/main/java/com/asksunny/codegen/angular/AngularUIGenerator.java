d063323a435c89f2a52dc7203458fb5360248996
SunnyLiu <sunnyliu2@gmail.com>
2016-04-03 17:32:12 -0400

    add index html

--- src/main/java/com/asksunny/codegen/angular/AngularUIGenerator.java
+++ src/main/java/com/asksunny/codegen/angular/AngularUIGenerator.java
@@ -4,13 +4,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.IOUtils;
 
 import com.asksunny.codegen.CodeGenConfig;
+import com.asksunny.codegen.CodeGenConfig.CodeOverwriteStrategy;
 import com.asksunny.codegen.CodeGenerator;
+import com.asksunny.codegen.TemplateRender;
+import com.asksunny.codegen.TypesBean;
 import com.asksunny.codegen.utils.ParamMapBuilder;
 import com.asksunny.codegen.utils.TemplateUtil;
 import com.asksunny.schema.Entity;
@@ -44,14 +48,14 @@ public class AngularUIGenerator extends CodeGenerator {
 
 		StringBuilder states = new StringBuilder();
 		StringBuilder navigations = new StringBuilder();
-		List<Entity> entities = schema.getAllEntities();		
+		List<Entity> entities = schema.getAllEntities();
 		for (Entity entity : entities) {
 			if (configuration.getIncludes().size() > 0 && !configuration.shouldInclude(entity.getName())) {
 				continue;
 			}
 			if (configuration.shouldIgnore(entity.getName())) {
 				continue;
-			}			
+			}
 			if (entity.isIgnoreView()) {
 				continue;
 			} else {
@@ -68,6 +72,10 @@ public class AngularUIGenerator extends CodeGenerator {
 				ParamMapBuilder.newBuilder().addMapEntry("APP_NAVIGATIONS", navigations.toString()).buildMap());
 		writeCode(navigationDir, "sidebar.html", navgiator);
 
+		String generated = TemplateRender.newInstance().setLoaderClass(getClass())
+				.setTemplate("index.html.ftl", Locale.US).addTemplateParam("config", configuration).renderTemplate();
+		writeCode(new File(configuration.getWebBaseSrcDir()), "index.html", generated);
+
 		String html = TemplateUtil.renderTemplate(
 				IOUtils.toString(getClass().getResourceAsStream("JScaffold.html.tmpl")),
 				ParamMapBuilder.newBuilder().addMapEntry("APP_NAVIGATIONS", navigations.toString()).buildMap());
@@ -81,7 +89,7 @@ public class AngularUIGenerator extends CodeGenerator {
 
 	protected void genEntityForm(StringBuilder states, StringBuilder navigations, Entity entity) throws IOException {
 		AngularEntityFormGenerator formGen = new AngularEntityFormGenerator(configuration, entity);
-		formGen.doCodeGen();		
+		formGen.doCodeGen();
 		states.append(formGen.genAngularState());
 		AngularEntityListGenerator listGen = new AngularEntityListGenerator(configuration, entity);
 		listGen.doCodeGen();
@@ -95,15 +103,15 @@ public class AngularUIGenerator extends CodeGenerator {
 		ZipInputStream zipin = new ZipInputStream(getClass().getResourceAsStream("/sbadmin-template.zip"));
 		try {
 			ZipEntry entry = null;
-			while ((entry = zipin.getNextEntry()) != null) {				
-				File path = new File(webappPath, entry.getName());			
+			while ((entry = zipin.getNextEntry()) != null) {
+				File path = new File(webappPath, entry.getName());
 				if (path.exists() || entry.isDirectory()) {
 					continue;
 				}
 				if (!path.getParentFile().exists() && !path.getParentFile().mkdirs()) {
 					throw new IOException("Failed to extract template, permission denied:" + path.getParentFile());
 				}
-				
+
 				FileOutputStream fout = new FileOutputStream(path);
 				try {
 					IOUtils.copy(zipin, fout);
